bc93911ed2b6204cabef1d687c750fbb15c9edc9
Huzaifa57 <shuzaifa222@gmail.com>
2025-06-28 19:51:20 +0500

    build fail fix

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -192,8 +192,9 @@ export default function Footer() {
             {links.map((link, index) => (
               <motion.div
                 key={link.id}
-                variants={staggerItem}
-                transition={{ delay: 0.9 + index * 0.05 }}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.9 + index * 0.05, ease: "easeOut" }}
                 whileHover={{ y: -2 }}
               >
                 <Link
